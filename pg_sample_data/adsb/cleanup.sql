2e492aa87970dd50a2468050d5f14a9a4f2fa9e1
Culver Stone <c_stone157@hotmail.com>
2022-05-13 08:54:03 -0700

    Somemore cleanup

--- pg_sample_data/adsb/cleanup.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE public.adsb_import;
-DROP SEQUENCE public.seq_adsb_uid;
--- pg_sample_data/pg_sample_data/adsb/020001-cleanup.sql
+++
@@ -1,5 +0,0 @@
-SET search_path TO sample_data;
-\connect sample_data
-
-DROP TABLE public.adsb_import;
-DROP SEQUENCE public.seq_adsb_uid;
